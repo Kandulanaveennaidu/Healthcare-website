fb420808dc7da921256b2fc59690f88d5c1b95d6
kandula Naveen <kandulanaveennaidu017@gmail.com>
2023-06-02 11:47:08 +0530

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+     apiKey: "AIzaSyC8VI9VrNjy-WCyS81TSQ_43FTnSIsgUrc",
+  authDomain: "chat-application-task.firebaseapp.com",
+  databaseURL: "https://chat-application-task-default-rtdb.firebaseio.com",
+  projectId: "chat-application-task",
+  storageBucket: "chat-application-task.appspot.com",
+  messagingSenderId: "608632635820",
+  appId: "1:608632635820:web:f9f48d167f6f254881ba09",
+  measurementId: "G-1F4JLWXMN3"
+  };
+  
+export default firebaseConfig;
